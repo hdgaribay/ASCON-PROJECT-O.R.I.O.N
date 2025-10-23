436b59d33a6d5aada9522921ac0436f32c6aa914
Hector Garibay <hectorgaribay2755@gmail.com>
2025-10-22 17:01:09 -0700

    Initial ASCON RTL drop

--- LINEAR/rotate.v
+++ LINEAR/rotate.v
@@ -2,7 +2,6 @@ module rotate(x, out, l);
 	input [63:0]x;
 	input [5:0]l;
 	output wire [63:0]out;
-	assign out = (x >>l)^(x<<(64-l));
-
+	assign out = (x >>l)|(x<<(64-l)); // or operation in instance that both bits are one
 endmodule
 
